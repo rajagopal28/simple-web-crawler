a86897a8b6a47f6d20bca5deed66ac8110fdd43f
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2019-12-16 15:32:34 +0530

    Adding few config strings

--- web-crawler/src/main/java/com/rm/monzo/app/util/CrawlerUtil.java
+++ web-crawler/src/main/java/com/rm/monzo/app/util/CrawlerUtil.java
@@ -9,6 +9,23 @@ public interface CrawlerUtil {
     String REPLACEABLE_EMPTY_STRING = "";
     int TEN_SECONDS_IN_MILLIS = 10000;
 
+    String UI_INTERFACE_TITLE = "Web Crawler Interface";
+    String UI_ENTER_URL_LABEL = "Enter URL:";
+    int UI_FRAME_WIDTH = 800;
+    int UI_FRAME_HEIGHT = 600;
+    String UI_DEFAULT_CRAWL_URL_VALUE = "https://www.google.com/s";
+    String UI_DEPTH_LABEL = "Depth:";
+    String UI_CRAWL_BUTTON_LABEL = "Crawl";
+    String UI_TREE_ROOT_NOTE_TEXT = "Crawled Sites";
+    String UI_DEFAULT_DEPTH_VALUE = "5";
+    int UI_URL_FIELD_MAX_LENGTH = 40;
+    int UI_DEPTH_FIELD_MAX_LENGTH = 5;
+    String UI_STATUS_VALUE_NONE_TEXT = "None";
+    String UI_STATUS_VALUE_CRAWLING_TEXT = "Crawling!!";
+    String UI_STATUS_ERROR_LABEL = "Error::";
+    String UI_STATUS_SUCCESS_LABEL = "Success!!";
+    String UI_STATUS_LABEL_TEXT = "Status:";
+
     static boolean isValidSameDomainURL(String currentURL, String nextURL) {
         String currentDomain = getAbsoluteURLWithoutInternalReference(currentURL)
                 .replaceAll(URL_REMOVABLE_PROTOCOL_PATTERN,REPLACEABLE_EMPTY_STRING)
